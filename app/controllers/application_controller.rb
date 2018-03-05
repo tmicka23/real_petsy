7bf8aede6323a60c59d05958ede38ccd494eaa7e
tmicka23 <mickathp42@gmail.com>
2018-03-05 17:27:51 +0100

    authentification par login fonctionnelle login = username ou email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,20 @@ class ApplicationController < ActionController::Base
 
   add_flash_types :success, :danger
 
+  before_action :configure_devise_parameters, if: :devise_controller?
+
+
+
+
+
+
+
+
+  private
+
+
+  def configure_devise_parameters
+  	devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+  end
+
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -36,7 +36,7 @@ Devise.setup do |config|
   # session. If you need permissions, you should implement that in a before filter.
   # You can also supply a hash where the value is a boolean determining whether
   # or not authentication should be aborted when the value is not present.
-  # config.authentication_keys = [:email]
+   config.authentication_keys = [:login]
 
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
@@ -141,11 +141,11 @@ Devise.setup do |config|
   config.reconfirmable = true
 
   # Defines which key will be used when confirming an account
-  # config.confirmation_keys = [:email]
+   config.confirmation_keys = [:email]
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
-  # config.remember_for = 2.weeks
+   config.remember_for = 2.weeks
 
   # Invalidates all the remember me tokens when the user signs out.
   config.expire_all_remember_me_on_sign_out = true
