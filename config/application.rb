f35c934913fe78c189533caf224ffadb00ea14b6
tmicka23 <mickathp42@gmail.com>
2018-03-05 15:10:31 +0100

    first commit

---
+++ config/application.rb
@@ -0,0 +1,30 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Petsy
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+        config.site = {
+
+      name: 'Petsy.fr'
+    }
+
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+    end
+    config.assets.paths << Rails.root.join('lib', 'assets', 'lib')
+  end
+end
